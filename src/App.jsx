14ff6dede976867a49b9720f4e9b36403ba48ad5
maevskyy <maevskijdimitrij1@gmail.com>
2023-11-20 23:15:42 +0200

    added functionality for auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
+	const [inputValue, setInputValue] = useState('');
+  const [queryData, setQueryData] = useState({name: '', password: ''})
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  return (
-    <>
-
+  useEffect(() => {
+    const query = window.location.search;
+    if (query) {    
+      function parseQueryString(url) {
+        const queryString = url.split('?')[1];
+        const paramsArray = queryString.split('&');
+      
+        const paramsObject = {};
+      
+        paramsArray.forEach(param => {
+          const [key, value] = param.split('=');
+          paramsObject[key] = value;
+        });
+      
+        return paramsObject;
+      }
     
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Введите текст"
-        className='test'
-      />
+      const queryParams = parseQueryString(query)
+      setQueryData(queryParams)
+      localStorage.setItem('userData', JSON.stringify(queryParams))
+    }
+  }, [])
 
-      <p>Вы ввели: {inputValue}</p>
-    </>
-  );
+
+
+
+	const handleInputChange = (event) => {
+		setInputValue(event.target.value);
+	};
+
+  const saveTextOnApi = () => {
+    fetch('http://localhost:8944/signIn/patch', {method: 'PATCH', body: JSON.stringify({userName: queryData.name, text: inputValue })})
+  }
+
+	return (
+		<>
+			<input
+				type='text'
+				value={inputValue}
+				onChange={handleInputChange}
+				placeholder='Введите текст'
+				className='test'
+			/>
+      <button onClick={saveTextOnApi}>
+        Save text
+      </button>
+			<p>{queryData.name}</p>
+			<p>Вы ввели: {inputValue}</p>
+		</>
+	);
 }
 
 export default App;
